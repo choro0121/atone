9735e62a2c12d878255434b968af0df1eb9c6821
Tosaki Takumi <tosaki.takumi@mail.canon>
2021-08-16 14:13:47 +0900

    add 'NotoSansJP' font

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
           appBarTheme: AppBarTheme(elevation: 0.0),
+          fontFamily: 'NotoSansJP',
         ),
         debugShowCheckedModeBanner: false,
         home: App(),
